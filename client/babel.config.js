92c693faa687273c112977b9169616c2c40166e3
Philippe <p.lee9519@gmail.com>
2022-11-09 21:33:40 -0500

    esting

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,12 +1,4 @@
-const plugins = [];
-
-// Instrument for code coverage in development mode
-if (process.env.NODE_ENV === "development") {
-  console.log(
-    "Detected development environment. Instrumenting code for coverage."
-  );
-  plugins.push("istanbul");
-}
+const plugins = ["istanbul"];
 
 module.exports = {
   presets: ["next/babel"],
